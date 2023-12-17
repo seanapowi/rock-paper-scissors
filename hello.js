20071d98fdc07d15120888ed4557e0eeeb3c7347
King William <seanapowi@gmail.com>
2023-12-17 16:58:49 +0300

    Added a prompt to get user input and computer with the computer's generated random result

--- hello.js
+++ hello.js
@@ -1,3 +1,6 @@
+let userImput = prompt("Hye there, please enter your option (either rock, paper or scissors)    ");
+userImput = userImput.toLocaleLowerCase();
+
 function getComputerChoice(){
     const randomNumber = Math.floor(Math.random()*3);
     console.log(randomNumber);
@@ -13,8 +16,43 @@ function getComputerChoice(){
     }
 }
 
-//We haven't called the fuction  here
+//We haven't called the fuction here
 function playRound(playerSelection, computerSelection){
 
-
+if(playerSelection === computerSelection){
+    return "It's a TIE, play again";
 }
+if(playerSelection === "rock"){
+    if(computerSelection === "paper"){
+        return "You Lose! Paper beats Rock";
+    }
+    else {
+        return "Congratulations, You win";
+    }
+}
+if(playerSelection === "paper") {
+    if(computerSelection === "scissors"){
+        return "You Lose! Scissors beats paper";
+    }
+    else {
+    return "Congratulations, You win";
+    }
+}
+if(playerSelection === "scissors"){
+    if(computerSelection === "rock"){
+        return "You Lose! Rock beats Scissors";
+    }
+    else{
+        return "Congratulations, You win";
+    }
+}
+}
+
+function game(){
+    const playerSelection = userImput;
+    const computerSelection = getComputerChoice();
+    console.log('playerSelection: ${playerSelection} VS. computerSelection: ${computerSelection}');
+    console.log(playRound(playerSelection, computerSelection));
+}
+
+game();
